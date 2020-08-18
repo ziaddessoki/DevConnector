8ad3edfc005df9741b5fe3ec9c2e6ed5fcc4f187
Ziad Eldesouky <ziad.dessoki@yahoo.com>
2020-08-18 01:15:35 -0400

    auth

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+
+const User = require("../../models/User");
 
 //@route GET api/auth
 //@ desc Test route
 //@access private
-router.get("/", (req, res) => res.send("auth route"));
+//to use the middleware just add the file as a second pramater and route will be protected
+router.get("/", auth, async (req, res) => {
+  try {
+    //since its a protected route n we use the token that has the ID
+    //in middelware we set the req.user to the user w/token
+    // not returning password in json
+    const user = await User.findById(req.user).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;

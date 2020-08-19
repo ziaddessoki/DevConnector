490a5828ef4422ef9a6fe2a195d7a99ddeec2d8d
Ziad Eldesouky <ziad.dessoki@yahoo.com>
2020-08-19 13:36:13 -0400

    profile.js/experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -208,6 +208,20 @@ router.put(
 //@route   DELETE api/profile/experience/:exp_id
 //@ desc   add all experience to profile
 //@access  private
-router.delete("/experience/:exp_id", auth, async (req, res) => {});
+router.delete("/experience/:exp_id", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+    //Get remove index
+    const removeIndex = profile.experience
+      .map((item) => item.id)
+      .indexOf(req.params.exp_id);
+    profile.experience.splice(removeIndex, 1);
+    await profile.save();
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;

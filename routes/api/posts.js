918cfb9929dca30d545a714c423b9adf6dd353a0
Ziad Eldesouky <ziad.dessoki@yahoo.com>
2020-08-21 23:18:57 -0400

    posts.likes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -109,6 +109,8 @@ router.put("/like/:id", auth, async (req, res) => {
   try {
     const post = await Posts.findById(req.params.id);
     //check if the post is already liked by logged user
+    //.filer will return an array ['id']
+    // if its length is > 0 (1) is already liked by the user
     if (
       post.likes.filter((like) => like.user.toString() === req.user.id).length >
       0
@@ -117,6 +119,34 @@ router.put("/like/:id", auth, async (req, res) => {
     }
     //add user to the likes array
     post.likes.unshift({ user: req.user.id });
+    await post.save();
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("server Error");
+  }
+});
+
+//@route PUT api/posts/unlike/:id
+//@ desc unlike a post
+//@access private
+router.put("/unlike/:id", auth, async (req, res) => {
+  try {
+    const post = await Posts.findById(req.params.id);
+    //check if the post is already liked by logged user
+    //.filter will return an array ['id']
+    // if its length is = 0, it was never liked by the user
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id)
+        .length === 0
+    ) {
+      return res.status(400).json({ msg: "Post was NOT liked" });
+    }
+    //add user to the likes array
+    const removeIndex = post.likes
+      .map((like) => like.user.toString())
+      .indexOf(req.user.id);
+    post.likes.splice(removeIndex, 1);
     await post.save();
     res.json(post.likes);
   } catch (err) {

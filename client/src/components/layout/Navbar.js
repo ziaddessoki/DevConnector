5b90416e648b5a90448227b4263d4017e4324ffd
Ziad Eldesouky <ziad.dessoki@yahoo.com>
2020-08-23 20:26:19 -0400

    clientSide

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -3,7 +3,6 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-
         <nav className="navbar bg-dark">
             <h1>
                 <a href="index.html"><i className="fas fa-code"></i> DevConnector</a>

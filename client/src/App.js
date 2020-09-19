8402af631d1dae0ea98b2235a6515a0c4bf3e393
Ziad Eldesouky <ziad.dessoki@yahoo.com>
2020-09-19 02:33:12 -0400

    profile display

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import Dashboard from "./components/dashboard/Dashboard";
 import PrivateRoute from "./components/routing/PrivateRoute";
 import CreateProfile from "./components/profile-form/CreateProfile";
 import Profiles from "./components/profiles/Profiles";
+import Profile from "./components/profile/Profile";
 import EditProfile from "./components/profile-form/Edit-profile";
 import AddExperience from "./components/profile-form/AddExperience";
 import AddEducation from "./components/profile-form/AddEducation";
@@ -45,6 +46,7 @@ const App = () => {
               <Route exact path="/login" component={Login} />
               <Route exact path="/register" component={Register} />
               <Route exact path="/profiles" component={Profiles} />
+              <Route exact path="/profile/:id" component={Profile} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
               <PrivateRoute
                 exact
---
+++ client/src/components/profile/profile.js
@@ -0,0 +1,42 @@
+import React, { useEffect, Fragment } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import Spinner from "../layout/Spinner";
+import { getProfileByID } from "../../actions/profile";
+
+const Profile = ({
+  getProfileByID,
+  profile: { profile, loading },
+  auth,
+  match,
+}) => {
+  useEffect(() => {
+    getProfileByID(match.params.id);
+  }, [getProfileByID, match.params.id]);
+  return (
+    <Fragment>
+      {profile === null || loading ? (
+        <Spinner />
+      ) : (
+        <Fragment>
+          <Link to="/profiles" className="btn btn-light">
+            Back to Profiles
+          </Link>
+        </Fragment>
+      )}
+    </Fragment>
+  );
+};
+
+Profile.propTypes = {
+  profile: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired,
+  getProfileByID: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  profile: state.profile,
+  auth: state.auth,
+});
+export default connect(mapStateToProps, { getProfileByID })(Profile);

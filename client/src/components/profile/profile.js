a354dc28aa4eea47f879bd1d585ea1ef2aa0b583
Ziad Eldesouky <ziad.dessoki@yahoo.com>
2020-09-19 04:50:08 -0400

    profile display

--- client/src/components/profile/profile.js
+++ client/src/components/profile/profile.js
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 import Spinner from "../layout/Spinner";
 import ProfileTop from "./ProfileTop";
 import ProfileAbout from "./ProfileAbout";
+import ProfileExperience from "./ProfileExperience";
 import { getProfileByID } from "../../actions/profile";
 
 const Profile = ({
@@ -35,6 +36,18 @@ const Profile = ({
           <div class="profile-grid my-1">
             <ProfileTop profile={profile} />
             <ProfileAbout profile={profile} />
+            <div className="profile-exp bg-white p-2">
+              <h2 className="text-primary">Experience</h2>
+              {profile.experience.length > 0 ? (
+                <Fragment>
+                  {profile.experience.map((exp) => (
+                    <ProfileExperience key={exp._id} experience={exp} />
+                  ))}
+                </Fragment>
+              ) : (
+                <h4>No experience credential</h4>
+              )}
+            </div>
           </div>
         </Fragment>
       )}

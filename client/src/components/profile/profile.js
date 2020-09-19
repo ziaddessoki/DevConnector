df5d639861cdc7e45a5e89a3438f8b42be8b4a91
Ziad Eldesouky <ziad.dessoki@yahoo.com>
2020-09-19 04:08:49 -0400

    profile display

--- client/src/components/profile/profile.js
+++ client/src/components/profile/profile.js
@@ -3,6 +3,8 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import Spinner from "../layout/Spinner";
+import ProfileTop from "./ProfileTop";
+import ProfileAbout from "./ProfileAbout";
 import { getProfileByID } from "../../actions/profile";
 
 const Profile = ({
@@ -23,6 +25,17 @@ const Profile = ({
           <Link to="/profiles" className="btn btn-light">
             Back to Profiles
           </Link>
+          {auth.isAuthenticated &&
+          auth.loading === false &&
+          auth.user._id === profile.user._id ? (
+            <Link to="/edit-profile" className="btn btn-dark">
+              Edit Profile
+            </Link>
+          ) : null}
+          <div class="profile-grid my-1">
+            <ProfileTop profile={profile} />
+            <ProfileAbout profile={profile} />
+          </div>
         </Fragment>
       )}
     </Fragment>

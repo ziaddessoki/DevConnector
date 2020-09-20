2b3c603bacfb392bea1f6c04bfffd9d034e9605f
Ziad Eldesouky <ziad.dessoki@yahoo.com>
2020-09-20 16:06:42 -0400

    comments

--- client/src/components/post/CommentItem.js
+++ client/src/components/post/CommentItem.js
@@ -3,10 +3,14 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import Moment from "react-moment";
+import { removeComment } from "../../actions/post";
+import auth from "../../reducers/auth";
 
 const CommentItem = ({
   postID,
   comment: { _id, text, name, avatar, user, date },
+  auth,
+  removeComment,
 }) => (
   <div class="post bg-white p-1 my-1">
     <div>
@@ -20,6 +24,15 @@ const CommentItem = ({
       <p class="post-date">
         Posted on : <Moment format="YYYY-MM-DD HH:mm">{date}</Moment>
       </p>
+      {!auth.loading && user === auth.user._id && (
+        <button
+          onClick={() => removeComment(postID, _id)}
+          className="btn btn-danger"
+          type="button"
+        >
+          <i className="fas fa-times" />
+        </button>
+      )}
     </div>
   </div>
 );
@@ -28,10 +41,11 @@ CommentItem.propTypes = {
   postID: PropTypes.number.isRequired,
   comment: PropTypes.object.isRequired,
   auth: PropTypes.object.isRequired,
+  removeComment: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   auth: state.auth,
 });
 
-export default connect(mapStateToProps, {})(CommentItem);
+export default connect(mapStateToProps, { removeComment })(CommentItem);

d262ec55f759ef92abded4d07ab0d2d19cd32d42
Ziad Eldesouky <ziad.dessoki@yahoo.com>
2020-09-20 15:24:58 -0400

    comments

--- client/src/components/post/CommentItem.js
+++ client/src/components/post/CommentItem.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { Fragment } from "react";
 import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import Moment from "react-moment";
 
-const CommentItem = ({ postID }) => {
-  return <div></div>;
+const CommentItem = ({
+  postID,
+  comment: { _id, text, name, avatar, user, date },
+}) => (
+  <div class="post bg-white p-1 my-1">
+    <div>
+      <Link to={`/profile/${user}`}>
+        <img class="round-img" src={avatar} alt="" />
+        <h4>{name}</h4>
+      </Link>
+    </div>
+    <div>
+      <p class="my-1">{text}</p>
+      <p class="post-date">
+        Posted on : <Moment format="YYYY-MM-DD HH:mm">{date}</Moment>
+      </p>
+    </div>
+  </div>
+);
+
+CommentItem.propTypes = {
+  postID: PropTypes.number.isRequired,
+  comment: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired,
 };
 
-CommentItem.propTypes = {};
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
 
-export default CommentItem;
+export default connect(mapStateToProps, {})(CommentItem);

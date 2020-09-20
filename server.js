f61cb1481794a4feed175dd1cc401a789b3afee4
Ziad Eldesouky <ziad.dessoki@yahoo.com>
2020-09-20 17:17:16 -0400

    Deployment

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   // server index.html
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "cllient", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
